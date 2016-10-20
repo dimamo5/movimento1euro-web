9a8c19c59908b8016a7140578027fa937fb66f16
dimamo5 <diogomoura13@gmail.com>
2016-10-20 19:32:34 +0100

    Travis config for staging server

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,16 +3,31 @@
 # print outputs and exit on first failure
 set -xe
 
-if [ $TRAVIS_BRANCH == "master" ] ; then
+if [ "{$TRAVIS_BRANCH}" == "master" ] ; then
 
     git checkout master
+    git config user.name "Travis CI"
+    git config user.email "travis@ldso.diogomoura.me"
     git remote add deploy ssh://root@ldso.diogomoura.me/var/repo/ldso.git
     rm -f .gitignore
     git add .
     git status # debug
     git commit -m "Deploy compressed files"
     git push -f deploy master
-    echo "d14618518m0ura"
+    echo "d14618518m0ura\n"
+
+elif [ "{$TRAVIS_BRANCH}" == "develop" ] ; then
+
+    git checkout master
+    git config user.name "Travis CI"
+    git config user.email "travis@ldso.diogomoura.me"
+    git remote add deploy ssh://root@ldso.diogomoura.me/var/repo_staging/ldso.git
+    rm -f .gitignore
+    git add .
+    git status # debug
+    git commit -m "Deploy compressed files"
+    git push -f deploy master
+    echo "d14618518m0ura\n"
 
 else
 
