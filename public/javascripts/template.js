b4e1363f2effe27396125e6bcf19861fd5a98520
IneSantos <inesscarneiro94@gmail.com>
2016-11-12 17:55:46 +0000

    Templates que o Diogo Fez e que nao foram para o git

---
+++ public/javascripts/template.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+    var table = new Vue({
+        el: '#table',
+        data: {
+            checkAll:false,
+            templates: []
+        },
+        created: function () {
+            $.get('/template/api', (data)=> {
+                for (var i = 0; i < data.templates.length; i++) {
+                    this.templates.push({select: false, id: data.templates[i].id, name: data.templates[i].name, content: data.templates[i].content});
+                }
+            })
+        },
+        watch:{
+            checkAll: function () {
+                for (var i = 0; i < this.templates.length; i++) {
+                    this.templates[i]['select'] = this.checkAll;
+                }
+            }
+        },
+        methods: {
+            remove: function () {
+                var url = '/template/api';
+                for (var i = 0; i < this.templates.length; i++) {
+                    if (this.templates[i].select) {
+                        var id = this.templates[i].id;
+                        $.ajax({
+                            url: url + id,
+                            type: 'DELETE',
+                            success: (data)=> {
+                                for (var i = 0; this.templates.length; i++) {
+                                    if (this.templates[i].id == data['removed']) {
+                                        this.templates.splice(i, 1);
+                                    }
+                                }
+                            }
+                        })
+                    }
+                }
+            }
+        }
+    });
+
+});
