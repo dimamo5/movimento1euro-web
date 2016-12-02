8c45ee91ede24379a03662dd10cef0a3a7923359
dimamo5 <diogomoura13@gmail.com>
2016-12-02 19:22:54 +0000

    basic layout for messages

--- public/javascripts/history.js
+++ public/javascripts/history.js
@@ -1,8 +1,30 @@
 $(document).ready(function () {
-    let locationSplited = window.location.href.split('/');
-    let location = locationSplited[locationSplited.length - 1];
-    if (location !== 'template' && location === 'user') {
-        var history = new Vue({});
-    }
+
+    var history = new Vue({
+        el: '#table',
+        data: {
+            messages: [],
+            search:''
+        },
+        mounted: function () {
+            $.get('/history/api/messages', (data) => {
+                for (let i = 0; i < data.length; i++) {
+                    this.messages.push({
+                        open: false,
+                        id: data[i].id,
+                        type: data[i].msg_type,
+                        title: data[i].title,
+                        content: data[i].content,
+                        date: data[i].date
+                    });
+                }
+            })
+        },
+        methods:{
+            getMessageInfo:function () {
+
+            }
+        }
+    });
 })
 ;
--- routes/history.js
+++ routes/history.js
@@ -1,18 +1,21 @@
-/**
- * Created by diogo on 30/11/2016.
- */
 const express = require('express');
 const db = require('../database/database');
 const api = require('../database/api_wrapper');
 const router = express.Router();
 
-/* GET Template listing. */
+/* GET Messages listing. */
 router.get('/', (req, res) => {
     res.render('history', {historyPage: true});
 });
 
 router.get('/api/messages', (req, res) => {
-
+    db.Message.findAll()
+        .then((messages) => {
+            res.json(messages);
+        })
+        .catch(() => {
+            res.status(404).end();
+        })
 });
 
 module.exports = router;
